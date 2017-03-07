ffbcbcc3ce3a24068440623dc3cde46bc3c8b2e5
Romain BULOT <romainbulot@gmail.com>
2017-03-07 19:13:00 +0100

    Ajout d'une seconde activité et de son appel depuis le bouton play

---
+++ android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/view/JeuActivity.java
@@ -0,0 +1,20 @@
+package wikiwhat.bulotsantini.dev.wikiwhat.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import wikiwhat.bulotsantini.dev.wikiwhat.R;
+
+/**
+ * Created by romain on 07/03/2017.
+ */
+
+public class JeuActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeu);
+    }
+}
--- android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/view/MainActivity.java
+++ android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/view/MainActivity.java
@@ -1,13 +1,21 @@
 package wikiwhat.bulotsantini.dev.wikiwhat.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import wikiwhat.bulotsantini.dev.wikiwhat.R;
 import wikiwhat.bulotsantini.dev.wikiwhat.presenter.MainPresenter;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void AfficherJeu(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, JeuActivity.class);
+        startActivity(intent);
+    }
 
     private MainPresenter mPresenter;
 
@@ -16,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 
     @Override
     protected void onStart() {
