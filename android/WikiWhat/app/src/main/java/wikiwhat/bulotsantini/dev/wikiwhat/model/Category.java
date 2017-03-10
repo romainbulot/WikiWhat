12c1a0140a9f512afbe1afc3a3f49bba4d83276c
Jonathan <santinijon@eisti.eu>
2017-03-10 18:36:33 +0100

    Ajout du modèle d'une catégorie

---
+++ android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/model/Category.java
@@ -0,0 +1,54 @@
+package wikiwhat.bulotsantini.dev.wikiwhat.model;
+
+/**
+ * Created by Administrator on 10/03/2017.
+ */
+
+public class Category {
+    private int id;
+    private String name;
+    private int nb_pages;
+
+    public Category(int id, String name, int nb_pages) {
+        this.id = id;
+        this.name = name;
+        this.nb_pages = nb_pages;
+    }
+
+    public Category(Category c) {
+        id = c.getId();
+        name = c.getName();
+        nb_pages = c.getNb_pages();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNb_pages() {
+        return nb_pages;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Category) {
+            Category c = (Category) o;
+            return c.getId() == getId();
+        }
+
+        return false;
+    }
+
+    public String toString() {
+        String res = name + " (" + nb_pages + " page";
+        if (nb_pages > 1) {
+            res += "s";
+        }
+        res += ")";
+
+        return res;
+    }
+}
--- android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/model/Game.java
+++ android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/model/Game.java
@@ -1,20 +1,19 @@
 package wikiwhat.bulotsantini.dev.wikiwhat.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 07/03/2017.
  */
 
 public class Game {
-    private int[] nb_pages_cat = new int[3];
-    private int nb_hint = 0;
-    private int final_score = 0;
+    private int score = 0;
+    private String current_page;
+    private List<String> hints = new ArrayList<>();
 
     public Game() {
 
     }
 
-    public Game(int nb_hint, int current_score) {
-        this.nb_hint = nb_hint;
-        this.final_score = current_score;
-    }
 }
