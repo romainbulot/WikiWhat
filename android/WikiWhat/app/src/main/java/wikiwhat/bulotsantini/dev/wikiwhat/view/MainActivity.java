d8e7d74ba763dcac491260328558be1cbba1fd77
Jonathan <santinijon@eisti.eu>
2017-03-10 19:53:15 +0100

    Ajout des éléments de l'interface dans l'activité principale

--- android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/view/MainActivity.java
+++ android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/view/MainActivity.java
@@ -5,24 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import wikiwhat.bulotsantini.dev.wikiwhat.R;
 import wikiwhat.bulotsantini.dev.wikiwhat.presenter.MainPresenter;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void DisplayGame(View view)
-    {
-        Intent intent = new Intent(MainActivity.this, JeuActivity.class);
-        startActivity(intent);
-    }
-
     private MainPresenter mPresenter;
+
+    // UI Elements
+    private Button mRefresh1;
+    private Button mRefresh2;
+    private Button mRefresh3;
+    private Button mRefreshAll;
+    private Button mPlay;
+
+    private TextView mCat1;
+    private TextView mCat2;
+    private TextView mCat3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCat1 = (TextView) findViewById(R.id.text_cat1);
+        mCat2 = (TextView) findViewById(R.id.text_cat2);
+        mCat3 = (TextView) findViewById(R.id.text_cat3);
+
+        mRefresh1 = (Button) findViewById(R.id.buttonRafraichir1);
+        mRefresh2 = (Button) findViewById(R.id.buttonRafraichir2);
+        mRefresh3 = (Button) findViewById(R.id.buttonRafraichir3);
+        mRefreshAll = (Button) findViewById(R.id.buttonToutRafraichir);
+        mPlay = (Button) findViewById(R.id.buttonJouer);
     }
 
     @Override
@@ -49,4 +66,10 @@ public class MainActivity extends AppCompatActivity {
     private void refreshAll() {
 
     }
+
+    public void DisplayGame(View view) {
+        Intent intent = new Intent(MainActivity.this, JeuActivity.class);
+        startActivity(intent);
+    }
+
 }
