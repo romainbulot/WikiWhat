88a398268d8613bfb0fd1d8ff33085ae971cc780
Jonathan <santinijon@eisti.eu>
2017-03-10 19:04:27 +0100

    Mise en place du presenter de main activity

--- android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/presenter/MainPresenter.java
+++ android/WikiWhat/app/src/main/java/wikiwhat/bulotsantini/dev/wikiwhat/presenter/MainPresenter.java
@@ -1,15 +1,32 @@
 package wikiwhat.bulotsantini.dev.wikiwhat.presenter;
 
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
 import wikiwhat.bulotsantini.dev.wikiwhat.view.MainActivity;
 
 /**
  * Created by Administrator on 07/03/2017.
  */
 
-public class MainPresenter {
+public class MainPresenter implements SingleObserver<String>{
     private MainActivity view;
 
     public MainPresenter(MainActivity v) {
         view = v;
+    }
+
+    @Override
+    public void onSubscribe(Disposable d) {
+        
+    }
+
+    @Override
+    public void onSuccess(String value) {
+
+    }
+
+    @Override
+    public void onError(Throwable e) {
+
     }
 }
